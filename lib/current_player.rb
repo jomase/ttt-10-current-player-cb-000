1d0f8858090c99d69caf975986494c123bb245ff
Johan Martin Seland <joma.seland@gmail.com>
2017-02-27 11:14:24 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+
+
+def turn_count(board)
+  board.count { |x| x == "X" || x == "O" }
+end 
+
+
+def current_player(board)
+  if turn_count(board).even? 
+    return "X"
+  else return "O"
+ end 
+end 
+
